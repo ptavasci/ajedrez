d8a86f38f50d0a08c8c8b475e95a5700b1576c21
Pablo Tavasci Dozo <ptavasci@gmail.com>
2025-07-14 00:26:01 -0300

    chore: add ESLint and Prettier configuration with scripts for linting and formatting
    - Updated package.json to include lint and format scripts.
    - Added ESLint and Prettier dependencies for code quality.
    - Created .prettierrc.cjs for Prettier configuration.
    - Added eslint.config.js for ESLint configuration with TypeScript and React support.

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import pluginPrettier from "eslint-plugin-prettier/recommended";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ignores: ["node_modules/", "android/", "dist/"],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 2021,
+        sourceType: "module",
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+  pluginPrettier,
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+    },
+  },
+];
