dad96dc9880c5a9667e0ec5a510724222b795ff0
Pablo Tavasci Dozo <ptavasci@gmail.com>
2025-07-19 21:04:54 -0300

    fix: badges

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,21 +7,24 @@ export const Footer: React.FC = () => {
   return (
     <footer className="mt-8 text-center text-lavender-mist text-sm opacity-75 flex justify-center items-center">
       <p className="mr-2">Hecho con ❤️ por Sofi para los amantes del ajedrez</p>
-      <div className="flex items-center space-x-1 hidden">
-        <span className="bg-gray-700 text-white px-2 py-1 rounded-full text-xs">
+      <div className="flex items-center space-x-1">
+        <span className="bg-gray-700 text-white px-1 py-0.5 rounded-full text-xs">
           {resolution.width}x{resolution.height}
         </span>
-        <span className="bg-gray-700 text-white px-2 py-1 rounded-full text-xs">
+        <span className="bg-gray-700 text-white px-1 py-0.5 rounded-full text-xs">
           {' '}
           {isMobile ? 'Mobile' : 'Desktop'}{' '}
         </span>
-        <span className="bg-gray-700 text-white px-2 py-1 rounded-full text-xs">
+        <span className="bg-gray-700 text-white px-1 py-0.5 rounded-full text-xs">
           {isTouch ? 'Touch' : 'No Touch'}
         </span>
-        <span className="bg-gray-700 text-white px-2 py-1 rounded-full text-xs">
+        <span className="bg-gray-700 text-white px-1 py-0.5 rounded-full text-xs">
           {isTV ? 'TV' : 'No TV'}
         </span>
-        <span className="bg-gray-700 text-white px-2 py-1 rounded-full text-xs">{platform}</span>
+        <span className="bg-gray-700 text-white px-1 py-0.5 rounded-full text-xs">{platform}</span>
+        <span className="bg-gray-700 text-white px-1 py-0.5 rounded-full text-xs">
+          {isTV ? 'TV (Tap)' : isTouch ? 'Tap' : 'D&D'}
+        </span>
       </div>
     </footer>
   );
